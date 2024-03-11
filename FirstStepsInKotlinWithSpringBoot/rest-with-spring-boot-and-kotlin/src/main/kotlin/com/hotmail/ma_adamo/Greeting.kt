64de2aa8c4a9cad034ed05344c542469bae56d1b
Matheus <ma_adamo@hotmail.com>
2024-03-11 13:39:58 -0300

    First steps in Kotlin with Spring Boot

---
+++ FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin/src/main/kotlin/com/hotmail/ma_adamo/Greeting.kt
@@ -0,0 +1,3 @@
+package com.hotmail.ma_adamo
+
+class Greeting (val id: Long, val content: String)
---
+++ FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin/src/main/kotlin/com/hotmail/ma_adamo/Startup.kt
@@ -0,0 +1,11 @@
+package com.hotmail.ma_adamo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
